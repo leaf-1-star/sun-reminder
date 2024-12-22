896a6e970e404a158b5874ee716eeb0261a64e88
leaf-1-star <1473508403@qq.com>
2024-12-22 21:00:47 +0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('sun-reminder-v1').then(function(cache) {
+            return cache.addAll([
+                'index.html',
+                'icon.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+}); 
